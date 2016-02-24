44ffb0d8857b245a4e13df85affab55e4a0bd89d
Michael Goldman <goldmandrummer@gmail.com>
2016-02-24 01:36:33 -0500

    redirect to not_found on non-existant product name

--- routes/products.js
+++ routes/products.js
@@ -11,10 +11,12 @@ router.get('/', function(req, res, next) {
 // This line is a template
 router.get('/:product', function(req, res, next) {
   var name = req.params.product;
-  var title = name + " - FeatureQuest ";
+  var title = name + ' - FeatureQuest ';
   var desc, id;
 
-  Products.orderByChild("name").equalTo(name).once('value', function(snap){
+  Products.orderByChild('name').equalTo(name).once('value', function(snap){
+    if (!snap.exists()) { res.render('not_found'); }
+
     for(wget in snap.val()){
       console.log(snap.val()[wget]);
       desc = snap.val()[wget].desc;
@@ -22,10 +24,10 @@ router.get('/:product', function(req, res, next) {
     }
 
     // res.render('product', { title: title, name : name , desc : desc, id : id, pageJs: 'product' });
-    console.log("ID: ", id);
-    console.log("Desc: ", desc);
-    console.log("Name: ", name);
-    console.log("Title: ", title);
+    console.log('ID: ', id);
+    console.log('Desc: ', desc);
+    console.log('Name: ', name);
+    console.log('Title: ', title);
     // firebase wurrty - Jeremy
     res.render('product', { title: title, name : name , desc : desc, id : id, pageJs: 'product' });
   });
